a1235b1a1198e53add15d0acf9277e2aa2f88ea1
Ahemd Adel <ahmed.adel.said@gmail.com>
2022-10-22 13:20:21 +0100

    Remove the setup function

--- app/src/androidTest/java/com/plcoding/calculatorprep/CalculatorActivityTest.kt
+++ app/src/androidTest/java/com/plcoding/calculatorprep/CalculatorActivityTest.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
